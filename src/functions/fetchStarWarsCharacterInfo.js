acce969f009265040cf2c43f6abe48f5084e422e
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2019-07-03 14:41:03 -0300

    Add lambda function example

---
+++ src/functions/fetchStarWarsCharacterInfo.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+exports.handler = async(event, context) => {
+  const { characterId } = event.queryStringParameters;
+  let response = {};
+
+  try {
+    const { status, data } = await axios({
+      url: `https://swapi.co/api/people/${characterId}`,
+      method: 'GET',
+    });
+    response = { character: data, success: status };
+  } catch (error) {
+    response.success = false;
+    response.error = error;
+    console.error(error);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      success: response.success,
+      data: response.data || response.error,
+    }),
+  };
+
+}
